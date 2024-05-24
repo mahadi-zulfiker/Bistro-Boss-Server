686054916d6e3530ad0d96608ef881785c437f5f
mahade <mahade.adib45@gmail.com>
2024-05-24 22:00:57 +0600

    existing user differntiate function added

--- index.js
+++ index.js
@@ -33,8 +33,13 @@ async function run() {
         const userCollection = client.db("BistroDB").collection("users");
 
         // users related api
-        app.post('/users', async(req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
